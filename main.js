8edd3a2d3f622068f4b15ee8677c41bbd84ef9a5
Behzadkk <40770751+Behzadkk@users.noreply.github.com>
2019-02-16 15:43:14 +0000

    seperate panel first

--- main.js
+++ main.js
@@ -1,29 +1,39 @@
 // slideshow
-let slideIndex = 1;
-showSlides(slideIndex);
+// let slideIndex = 1;
+// showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
+// // Next/previous controls
+// function plusSlides(n) {
+//   showSlides((slideIndex += n));
+// }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
+// function showSlides(n) {
+//   const slides = document.querySelectorAll(".slideshow__slide");
+//   if (n > slides.length) {
+//     slideIndex = 1;
+//   }
+//   if (n < 1) {
+//     slideIndex = slides.length;
+//   }
+//   for (let i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slides[slideIndex - 1].style.display = "block";
+// }
 
-function showSlides(n) {
-  let i;
-  const slides = document.getElementsByClassName("slideshow__slide");
-  if (n > slides.length) {
-    slideIndex = 1;
+// function currentPanel(id) {
+//   const panel = document.querySelectorAll(".panel__slide");
+//   panel.addEventListener("click", function() {
+//     let id = panel.;
+//     showPanel(id);
+//   });
+// }
+let id = "dimensions";
+showPanel(id);
+function showPanel(id) {
+  const panelSlide = document.querySelectorAll(".panel__slide");
+  for (let i = 0; i < panelSlide.length; i++) {
+    panelSlide[i].style.display = "none";
   }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+  document.getElementById(id).style.display = "block";
 }
